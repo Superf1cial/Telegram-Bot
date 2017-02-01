7b006066d0ace1acde9c1375064d6ee0c6312ae4
Marvin Schröder <overwatchanalyst@gmail.com>
2017-02-01 09:36:09 +0100

    Update Telegrambot.py

--- Telegrambot.py
+++ Telegrambot.py
@@ -10,8 +10,6 @@ print("Setting up Config")
 WAIT = 15
 ERROR_WAIT = 20
 MAXPOSTS = 100
-URL_LIST = []
-SUBREDDIT = "memes"
 
 ''' SETUP COMPLETED'''
 
@@ -27,44 +25,67 @@ bot = telegram.Bot(token="304300939:AAGOQCXUuA4laeKEZkJuwppQaE-kosTGzxQ")
 updater = Updater(token = '304300939:AAGOQCXUuA4laeKEZkJuwppQaE-kosTGzxQ')
 dispatcher = updater.dispatcher
 
-def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='''Available commands:
-    
-    /pic
-    /subreddits
-    /setsubreddit
-
-    ''')
-
-def Send_Photo(bot, update, SUBREDDIT):
+def parse_reddit(SUBREDDIT):
+    global URL_LIST
     s = r.get_subreddit(SUBREDDIT)
-    print(s)
-    for submission in s.get_hot(limit=25):
+    URL_LIST = []
+    for submission in s.get_top(limit=25):
         URL = submission.url
         URL_LIST.append(URL)
-    bot.sendPhoto(chat_id=update.message.chat_id, photo=random.choice(URL_LIST))
+    return URL_LIST
+
+def Current_Subreddit(bot, update):
+    print(URL_LIST)
+    bot.sendMessage(chat_id=update.message.chat_id, text='''Status:
+    {}
+    '''.format(SUBREDDIT))
+
+def Send_Photo(bot, update):
+    REDDIT_URLS = parse_reddit(SUBREDDIT)
+    file = random.choice(REDDIT_URLS)
+    print(file)
+    if ".gifv" in file or "gfycat" in file:
+        print("Ich will ein Gif schicken")
+        bot.sendDocument(chat_id=update.message.chat_id, document=file)
+    else:
+        print("Ich will ein Foto schicken")
+        bot.sendPhoto(chat_id=update.message.chat_id, photo=file)
 
 def Available_Subreddits(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text= '''
-/r/memes
-/r/funny
-/r/pics
-/r/funnypictures
-/r/woahdude
-/r/wallpapers
-/r/techsupportgore''')
+You can set any Subreddit you want, just make sure it actually has images on it.
+You can set a subreddit by doing the following:
+
+/setsubreddit memes
+
+Change memes for whatever subreddit you would like to set.
+After that just use /getImage to post a Image off Reddit''')
 
 def Set_Subreddit(bot, update, args):
+    global SUBREDDIT
     for chosensubreddit in args:
         SUBREDDIT = chosensubreddit
         print(SUBREDDIT)
         URL_LIST = []
-    bot.sendMessage(chat_id=update.message.chat_id, text= "Subreddit set to {}".format(chosensubreddit))
-    return SUBREDDIT
+    bot.sendMessage(chat_id=update.message.chat_id, text= "Subreddit set to {}!".format(chosensubreddit))
+    return SUBREDDIT, URL_LIST
 
-updater.dispatcher.add_handler(CommandHandler('status', start))
-updater.dispatcher.add_handler(CommandHandler('pic', Send_Photo))
-updater.dispatcher.add_handler(CommandHandler('subreddits', Available_Subreddits))
+def start(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text='''
+To start using me, use the command /HowTo
+
+Available commands:
+/start
+/howto
+/currentsubreddit
+/getimage
+/setsubreddit''')
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('currentsubreddit', Current_Subreddit))
+updater.dispatcher.add_handler(CommandHandler('getimage', Send_Photo))
+updater.dispatcher.add_handler(CommandHandler('howto', Available_Subreddits))
 updater.dispatcher.add_handler(CommandHandler('setsubreddit', Set_Subreddit, pass_args = True))
 
 updater.start_polling()
